985e0800e15cf8cc32462361c5fe00cd3617a98f
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:43:10 +0530

    Update ScheduleService.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduleService.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduleService.java
@@ -2,12 +2,14 @@ package com.capg.fms.service;
 
 import java.util.List;
 
+import com.capg.fms.exceptions.ScheduleIdExistsException;
+import com.capg.fms.exceptions.ScheduleIdNotFoundException;
 import com.capg.fms.model.Schedule;
 
 public interface ScheduleService {
-	public Schedule newSchedule(Schedule schedule);
-	public Schedule getSchedule(int scheduleid);
+	public Schedule newSchedule(Schedule schedule) throws ScheduleIdExistsException;
+	public Schedule getSchedule(int scheduleid) throws ScheduleIdNotFoundException;
 	public List<Schedule> getAllSchedule();
-	public Schedule modifySchedule(Schedule schedule);
+	public boolean modifySchedule(Schedule schedule);
 	public boolean deleteSchedule(int scheduleid);
 }
