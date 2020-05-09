fc7fc0d461b28f6c42fde9e6f1cb268442312a6b
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:28:01 +0530

    Update ScheduleController.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/controller/ScheduleController.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/controller/ScheduleController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capg.fms.exceptions.ScheduleIdExistsException;
+import com.capg.fms.exceptions.ScheduleIdNotFoundException;
 import com.capg.fms.model.Schedule;
 import com.capg.fms.service.ScheduleService;
 
@@ -22,27 +24,67 @@ public class ScheduleController {
 	@Autowired
 	private ScheduleService scheduleService;
 	
-	@GetMapping(value="/schedule/{scheduleId}",produces={"application/json"})
-	public List<Schedule> displaySchedule(@PathVariable int scheduleId){
-		return scheduleService.getAllSchedule(scheduleId);
+	/* Method 	  : getScheduleById
+	 * Description: This is a GetMethod(Http) by "scheduleId" is used to get the schedule from the database.
+	 * Type 	  : ResponseEntity<List<Schedule>>
+	 * Parameters : scheduleId
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@GetMapping("/getScheduleById/{scheduleId}")
+	public Schedule getScheduleById(@PathVariable int scheduleId) throws ScheduleIdNotFoundException{
+		return scheduleService.getSchedule(scheduleId);
 	}
 	
-	@PostMapping(value="/schedule/add",produces= {"application/json"})
-	public String addSchedule(@RequestBody Schedule schedule) {
-		scheduleService.newSchedule(schedule);
-		return "Schedule added successfully";
+	/* Method 	  : getAllSchedule
+	 * Description: This is a GetMethod(Http) by "scheduleId" is used to get all schedules from the database.
+	 * Type 	  : ResponseEntity<List<Schedule>>
+	 * Parameters : -
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@GetMapping("/getAllSchedule")
+	public List<Schedule> getAllSchedule(){
+		return scheduleService.getAllSchedule();
+		
 	}
 	
-	@PutMapping(value="/schedule/update",consumes= {"application/json"})
+	/* Method 	  : addSchedule
+	 * Description: This is a PostMethod(Http) is used to add the schedule in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : Schedule(Class)
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@PostMapping(value="/addSchedule")
+	public Schedule addSchedule(@RequestBody Schedule schedule) throws ScheduleIdExistsException {
+		return scheduleService.newSchedule(schedule);
+		}
+	
+	/* Method 	  : updateSchedule
+	 * Description: This is a PutMethod(Http) is used to update the schedule in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : Schedule(Class)
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@PutMapping("/updateSchedule")
 	public String updateSchedule(@RequestBody Schedule schedule) {
 		scheduleService.modifySchedule(schedule);
 		return "Schedule modified successfully";
 	}
 	
-	@DeleteMapping(value="/schedule/delete/{scheduleId}")
-	public String deleteSchedule(@PathVariable int scheduleId) {
-		scheduleService.deleteSchedule(scheduleId);
-		return "Schedule with Schedule Id:"+scheduleId+"deleted successfully";
+	/* Method 	  : deleteSchedule
+	 * Description: This is a DeleteMethod(Http) by "scheduleId" is used to delete the schedule in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : scheduleId
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@DeleteMapping("/deleteSchedule/{scheduleId}")
+	public boolean deleteSchedule(@PathVariable int scheduleId) {
+		return scheduleService.deleteSchedule(scheduleId);
+	
 	}
 	
 
