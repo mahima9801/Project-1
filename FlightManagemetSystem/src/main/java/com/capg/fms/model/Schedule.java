013793238ab34c2aea627a64add221ae73e4473d
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:42:00 +0530

    Update Schedule.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/model/Schedule.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/model/Schedule.java
@@ -2,65 +2,96 @@ package com.capg.fms.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Size;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Schedule")
 public class Schedule {
 	@Id
-	@Size(min=4,max=8)
-	private int schedule_id;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name="Schedule_Id")
+	private int scheduleId;
+	
+	@Column(name="Source")
 	private String source;
+	
+	@Column(name="Destination")
 	private String destination;
+	
+	@Column(name="Arrival_Date_Time")
 	private LocalDateTime arrivaltime;
+	
+	@Column(name="Departure_Date_Time")
 	private LocalDateTime departuretime;
+	
+
 	public Schedule() {
 		super();
 	}
-	public Schedule(int schedule_id, String source, String destination, LocalDateTime arrivaltime,
+
+	public Schedule(int scheduleId, String source, String destination, LocalDateTime arrivaltime,
 			LocalDateTime departuretime) {
 		super();
-		this.schedule_id = schedule_id;
+		this.scheduleId = scheduleId;
 		this.source = source;
 		this.destination = destination;
 		this.arrivaltime = arrivaltime;
 		this.departuretime = departuretime;
 	}
-	public int getSchedule_id() {
-		return schedule_id;
+
+	public int getScheduleId() {
+		return scheduleId;
 	}
-	public void setSchedule_id(int schedule_id) {
-		this.schedule_id = schedule_id;
+
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
 	}
+
 	public String getSource() {
 		return source;
 	}
+
 	public void setSource(String source) {
 		this.source = source;
 	}
+
 	public String getDestination() {
 		return destination;
 	}
+
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
+
 	public LocalDateTime getArrivaltime() {
 		return arrivaltime;
 	}
+
 	public void setArrivaltime(LocalDateTime arrivaltime) {
 		this.arrivaltime = arrivaltime;
 	}
+
 	public LocalDateTime getDeparturetime() {
 		return departuretime;
 	}
+
 	public void setDeparturetime(LocalDateTime departuretime) {
 		this.departuretime = departuretime;
 	}
+
 	@Override
 	public String toString() {
-		return "Schedule [schedule_id=" + schedule_id + ", source=" + source + ", destination=" + destination
+		return "Schedule [scheduleId=" + scheduleId + ", source=" + source + ", destination=" + destination
 				+ ", arrivaltime=" + arrivaltime + ", departuretime=" + departuretime + "]";
 	}
+
+	
+	
+	
 
 }
