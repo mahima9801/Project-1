b689f165a6176c90b4c9759b32349899855f8924
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:43:33 +0530

    Update ScheduleServiceImpl.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduleServiceImpl.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.capg.fms.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capg.fms.dao.ScheduleDao;
+import com.capg.fms.exceptions.ScheduleIdExistsException;
+import com.capg.fms.exceptions.ScheduleIdNotFoundException;
 import com.capg.fms.model.Schedule;
 
 
@@ -18,28 +21,75 @@ public class ScheduleServiceImpl implements ScheduleService{
 	@Autowired
 	private ScheduleDao scheduleDao;
 
+	/* Method:newSchedule
+	 * This method is used to add the schedule in the Schedule table.
+	 * Type:boolean
+	 * Parameter:Schedule
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
-	public Schedule newSchedule(Schedule schedule) {
-		scheduleDao.addSchedule(schedule);
-		return schedule;
+	public Schedule newSchedule(Schedule schedule) throws ScheduleIdExistsException{
+		List<Schedule> list = getAllSchedule();
+		Optional<Schedule> optional = list.stream().filter(f1->f1.getScheduleId()==schedule.getScheduleId()).findFirst();
+		if(optional.isPresent()) {
+			return scheduleDao.addSchedule(schedule);
+		}
+		else
+			throw new ScheduleIdExistsException("This id is already taken");
 	}
 
+	/* Method:getSchedule
+	 * This method is used to retrieve the schedule from the Schedule table.
+	 * Type:List<Schedule>
+	 * Parameter:scheduleId
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
-	public Schedule getSchedule(int scheduleid) {
-		return scheduleDao.retrieveSchedule(scheduleid);
+	public Schedule getSchedule(int scheduleId) throws ScheduleIdNotFoundException{
+		List<Schedule> list  = getAllSchedule();
+		Optional<Schedule> optional = list.stream().filter(f1->f1.getScheduleId()==scheduleId).findFirst();
+		if(optional.isPresent()) {
+			return scheduleDao.retrieveSchedule(scheduleId);
+	}
+		else
+			throw new ScheduleIdNotFoundException("Id doesn't exist");
+			
 	}
 
+	/* Method:getAllSchedule
+	 * This method is used to retrieve all schedules from the Schedule table.
+	 * Type:List<Schedule>
+	 * Parameter:-
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public List<Schedule> getAllSchedule() {
 		return scheduleDao.retrieveAllSchedule();
 	}
 
+	/* Method:modifySchedule
+	 * This method is used to update the schedule in the Schedule table.
+	 * Type:boolean
+	 * Parameter:Schedule
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */	
 	@Override
-	public Schedule modifySchedule(Schedule schedule) {
+	public boolean modifySchedule(Schedule schedule) {
 		scheduleDao.updateSchedule(schedule);
-		return schedule;
+		return true;
 	}
 
+	/* Method:deleteSchedule
+	 * This method is used to delete the schedule in the Schedule table.
+	 * Type:boolean
+	 * Parameter:scheduleId
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public boolean deleteSchedule(int scheduleid) {
 		return scheduleDao.deleteSchedule(scheduleid);
