cd75a90dd85df76e348923fef4b71b2fcd6f71de
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:44:32 +0530

    Update ScheduledFlightsServiceImpl.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduledFlightsServiceImpl.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/service/ScheduledFlightsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.capg.fms.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.capg.fms.dao.ScheduledFlightsDao;
+import com.capg.fms.exceptions.ScheduleIdExistsException;
+import com.capg.fms.exceptions.ScheduleIdNotFoundException;
 import com.capg.fms.model.Schedule;
 import com.capg.fms.model.ScheduledFlights;
 
@@ -18,42 +21,85 @@ public class ScheduledFlightsServiceImpl implements ScheduledFlightsService{
 	@Autowired
 	private ScheduledFlightsDao scheduledFlightsDao;
 
+	/* Method:newcheduledFlights
+	 * This method is used to add the scheduled flight in the ScheduledFlights table.
+	 * Type:boolean
+	 * Parameter:sflight
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
-	public boolean newScheduledFlights(ScheduledFlights sflight) throws ScheduleIdNotFoundException {
+	public boolean newScheduledFlights(ScheduledFlights sflight) throws ScheduleIdExistsException {
 		List<ScheduledFlights> list = getAllScheduledFlights();
-		Optional<ScheduledFlights> optional = list.stream().filter(f1->f1.getScheduleId()==sflight.getScheduleId()).findFirst();
+		Optional<ScheduledFlights> optional = list.stream().filter(f1->f1.getSchedule()==sflight.getSchedule()).findFirst();
 		if(optional.isPresent()) {
 			return scheduledFlightsDao.addScheduledFlights(sflight);
 		}
 		else
-			throw new ScheduleIdNotFoundException("This id is already taken");
+			throw new ScheduleIdExistsException("This id is already taken");
 	}
 
+	/* Method:getScheduledFlights
+	 * This method is used to retrieve the scheduled flight from the Schedule, ScheduledFlights and Flight table.
+	 * Type:List<Schedule>
+	 * Parameters:source and destination
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public List<Schedule> getScheduledFlight(String source, String destination) {
 		return scheduledFlightsDao.retrieveScheduledFlights(source, destination);
 	}
 	
+	/* Method:getAllScheduledFlights
+	 * This method is used to retrieve all scheduled flight from the ScheduledFlights table.
+	 * Type:List<ScheduledFlights>
+	 * Parameter:-
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public List<ScheduledFlights> getAllScheduledFlights() {
-		return scheduledFlightsDao.retrieveAllShceduledFlights();
+		return scheduledFlightsDao.retrieveAllScheduledFlights();
 	}
 	
+	/* Method:getScheduledFlight
+	 * This method is used to retrieve the scheduled flight from the ScheduledFlights table.
+	 * Type:List<ScheduledFlights>
+	 * Parameter:scheduleId
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public List<ScheduledFlights> getScheduledFlight(int scheduleId) throws ScheduleIdNotFoundException {
 		List<ScheduledFlights> list = getAllScheduledFlights();
-		Optional<ScheduledFlights> optional = list.stream().filter(f1->f1.getScheduleId().getScheduleId()==scheduleId).findFirst();
+		Optional<ScheduledFlights> optional = list.stream().filter(f1->f1.getSchedule().getScheduleId()==scheduleId).findFirst();
 		if(optional.isPresent()) {
-			return scheduledFlightsDao.retrieveAllScheduledFlights(scheduleId);
+			return scheduledFlightsDao.retrieveScheduledFlight(scheduleId);
 		}
 		else
 			throw new ScheduleIdNotFoundException("Id doesn't exist");
 	}
+	
+	/* Method:modifyScheduledFlights
+	 * This method is used to update the scheduled flight in the ScheduledFlights table.
+	 * Type:boolean
+	 * Parameter:sflight
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public boolean modifyScheduledFlights(ScheduledFlights sflight) {
 		return scheduledFlightsDao.updateScheduledFlights(sflight);
 	}
 
+	/* Method:deleteScheduledFlights
+	 * This method is used to delete the scheduled flight in the ScheduledFlights table.
+	 * Type:boolean
+	 * Parameter:scheduleId
+	 * Author Name:Mahima Mishra
+	 * Version:
+	 */
 	@Override
 	public boolean deleteScheduledFlights(int scheduleId) {
 		return scheduledFlightsDao.deleteScheduledFlights(scheduleId);
