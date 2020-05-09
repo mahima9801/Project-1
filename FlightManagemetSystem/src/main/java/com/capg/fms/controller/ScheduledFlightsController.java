935f757b24ccf61f6d8bc0c78df9385afe7fdf3d
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:28:57 +0530

    Update ScheduledFlightsController.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/controller/ScheduledFlightsController.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/controller/ScheduledFlightsController.java
@@ -2,7 +2,10 @@ package com.capg.fms.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,10 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.capg.fms.exception.ScheduleIdNotFoundException;
+import com.capg.fms.exceptions.ScheduleIdExistsException;
+import com.capg.fms.exceptions.ScheduleIdNotFoundException;
 import com.capg.fms.model.Schedule;
 import com.capg.fms.model.ScheduledFlights;
 import com.capg.fms.service.ScheduledFlightsService;
@@ -21,41 +24,82 @@ import com.capg.fms.service.ScheduledFlightsService;
 @RestController
 @RequestMapping("/scheduledFlights")
 public class ScheduledFlightsController {
-	
 	@Autowired
 	private ScheduledFlightsService scheduledFlightsService;
 	
-	@GetMapping(value="/scheduledFlight/{scheduleId}",produces= {"application/json"})
-	public List<ScheduledFlights> viewSchdeuledFlights(@PathVariable int scheduleId) throws ScheduleIdNotFoundException{
+	/* Method 	  : getScheduledFlightById
+	 * Description: This is a GetMethod(Http) by "scheduleId" is used to get the scheduled flight from the database.
+	 * Type 	  : ResponseEntity<List<ScheduledFlights>>
+	 * Parameters : scheduleId
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@GetMapping("/getScheduledFlightById/{scheduleId}")
+	public List<ScheduledFlights> getScheduledFlightById(@PathVariable int scheduleId) throws ScheduleIdNotFoundException{
 		return scheduledFlightsService.getScheduledFlight(scheduleId);
 	}
-	
-	@GetMapping(value="/scheduledFlights")
-	public List<ScheduledFlights> viewAllScheduledFlights(){
+	//
+	/* Method 	  : getAllScheduledFlights
+	 * Description: This is a GetMethod(Http) is used to get all scheduled flights from the database.
+	 * Type 	  : ResponseEntity<List<ScheduledFlights>>
+	 * Parameters : -
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@GetMapping("/flights")
+	public List<ScheduledFlights> getAllScheduledFlights(){
 		return scheduledFlightsService.getAllScheduledFlights();
 	}
 	
-	@GetMapping(value="/displaySchedule")
-	public List<Schedule> viewFlightSchedule(@RequestParam("src") String source,@RequestParam("dest") String destination) {
+	/* Method 	  : getFlightSchedule
+	 * Description: This is a GetMethod(Http) by "source" and "destination" is used to get all scheduled flights from the database.
+	 * Type 	  : ResponseEntity<List<Schedule>>
+	 * Parameters : source and destination
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@GetMapping("/getFlightSchedule")
+	public List<Schedule> getFlightSchedule(@PathVariable String source,@PathVariable String destination) {
 		return scheduledFlightsService.getScheduledFlight(source, destination);
 	}
 	
-	@PostMapping(value="/scheduledFlight/add",produces= {"application/json"})
-	public String addScheduledFlight(@RequestBody ScheduledFlights sflight) throws ScheduleIdNotFoundException {
+	/* Method 	  : addScheduledFlight
+	 * Description: This is a PostMethod(Http) is used to add the scheduled flight in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : Schedule(Class)
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@PostMapping("/addScheduledFlight")
+	public String addScheduledFlight(@RequestBody ScheduledFlights sflight) throws ScheduleIdExistsException {
 		scheduledFlightsService.newScheduledFlights(sflight);
 		return "Flight Schedule added successfully";
 	}
 	
-	@PutMapping(value="/schdeuledFlight/modify",consumes= {"application/json"})
+	/* Method 	  : modifyScheduledFlight
+	 * Description: This is a PutMethod(Http) is used to update the scheduled flight in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : ScheduledFlights(Class)
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@PutMapping("/modifySchdeuledFlight")
 	public String modifyScheduledFlight(@RequestBody ScheduledFlights sflight) {
 		scheduledFlightsService.modifyScheduledFlights(sflight);
-		return "Flight Schdeule modified successfully";
+		return "Flight Schedule modified successfully";
 	}
 	
-	@DeleteMapping(value="/scheduledFlight/delete/{scheduleId}")
-	public String deleteScheduledFlight(@PathVariable int scheduleId) {
+	/* Method 	  : deleteScheduledFlight
+	 * Description: This is a DeleteMethod(Http) by "scheduleId" is used to delete the scheduled flight in the database.
+	 * Type 	  : ResponseEntity<String>
+	 * Parameters : scheduleId
+	 * Author 	  : Mahima Mishra
+	 * Version 	  : 
+	 */
+	@DeleteMapping("/deleteScheduledFlight/{scheduleId}")
+	public ResponseEntity<String> deleteScheduledFlight(@PathVariable int scheduleId) {
 		scheduledFlightsService.deleteScheduledFlights(scheduleId);
-		return "Flight Schedule with schedule Id:"+scheduleId+"deleted successfully";
+		return new ResponseEntity<String>("Flight Schedule with schedule Id:"+scheduleId+"deleted successfully",HttpStatus.OK);
 	}
 	
 	
