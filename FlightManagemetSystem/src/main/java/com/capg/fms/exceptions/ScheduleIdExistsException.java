d27e7e91a705c250ce587a8d01a0fcc67b7adc7e
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-09 18:40:47 +0530

    Add files via upload

---
+++ FlightManagemetSystem/src/main/java/com/capg/fms/exceptions/ScheduleIdExistsException.java
@@ -0,0 +1,9 @@
+package com.capg.fms.exceptions;
+
+@SuppressWarnings("serial")
+public class ScheduleIdExistsException extends Exception{
+	public ScheduleIdExistsException(String msg) {
+		super();
+	}
+
+}
